25c694f110f1cf64f37e1e56551e43ddd14591cb
nafise <nafise.mougamadousalime@gmail.com>
2024-01-19 11:29:47 +0100

    hover graphique fini

--- crypto-watch/src/components/Table.js
+++ crypto-watch/src/components/Table.js
@@ -147,7 +147,9 @@ const Table = ({ coinsData }) => {
                 return null;
             }
           })
-          .map((coin, index) => <TableLine coin={coin} index={index} />)}
+          .map((coin, index) => (
+            <TableLine coin={coin} index={index} key={index} />
+          ))}
     </div>
   );
 };
